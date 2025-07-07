16d1466235e361fce20e46510c0b3a1eecb1c8b1
hardeek-sharma <hardeeksa@gmail.com>
2025-07-06 23:52:51 -0400

    answered questions, added #defines to make answers interactive

---
+++ quizAnswers/chapter4/lesson12/main.cpp
@@ -0,0 +1,85 @@
+#define null
+
+/*
+Question #1 |||| Correct
+    Write a short program where the user is asked to enter a single character. Print the value of the character and its ASCII code, using static_cast.
+    The program’s output should match the following:
+
+        Enter a single character: a
+        You entered 'a', which has ASCII code 97.
+
+Answer: */
+    #ifdef q1
+
+        #include <iostream>
+
+        int main()
+        {
+            std::cout << "Enter a single character: ";
+            char ch {};
+            std::cin >> ch;
+
+            std::cout << "You entered " << ch << " Which has a value of " << static_cast<int>(ch) << ".\n";
+
+            return 0;
+        }
+
+    #endif
+
+/*
+Question #2 |||| Correct
+    Modify the program you wrote for quiz #1 to use implicit type conversion instead of static_cast. How many different ways can you think of to do this?
+    Note: You should favor explicit conversions over implicit conversions, so don’t actually do this in real programs -- this is just to test your understanding of where implicit conversions can occur.
+
+Answer: */
+
+    #ifdef q2_0
+
+        #include <iostream>
+
+        int main()
+        {
+            std::cout << "Enter a single character: ";
+            char ch {};
+            std::cin >> ch;
+            int n {ch};
+
+            std::cout << "You entered " << ch << " Which has a value of " << n << ".\n";
+
+            return 0;
+        }
+
+    #endif
+
+//BORDER FOR VISUAL
+
+    #ifdef q2_1
+
+    #include <iostream>
+
+    int main()
+    {
+        // Only works if warning to error is off
+        std::cout << "Enter a single character: ";
+        int n {};
+        std::cin >> n;
+        char ch {n};
+
+        std::cout << "You entered " << ch << " Which has a value of " << n << ".\n";
+
+        return 0;
+        }
+
+    #endif
+
+
+
+
+#ifdef null
+
+int main()
+{
+    return 0;
+}
+
+#endif
