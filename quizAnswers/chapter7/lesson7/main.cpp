00ebbe3a2e7cd362fe4026524ea0fc9ca6fdaf58
hardeek-sharma <hardeeksa@gmail.com>
2025-07-16 21:31:15 -0400

    answered question 7.7

---
+++ quizAnswers/chapter7/lesson7/main.cpp
@@ -0,0 +1,8 @@
+/*
+Question #1
+    What’s the difference between a variable’s scope, duration, and linkage? 
+    What kind of scope, duration, and linkage do global variables have?
+Answer: 
+    Scope is where a variable is accesibl. duration is the how long the variable lives. Linkage tells if the variable can be used in other places(like different files).
+    global variables have global scope, static duration (lasts until the file ends), and either internal (single file), or external(multiple files) linkage.
+*/
