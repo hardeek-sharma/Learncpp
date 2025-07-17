e5b6206ac56e6dd757032f88ce024cc62b327ceb
hardeek-sharma <hardeeksa@gmail.com>
2025-07-16 22:23:06 -0400

    made lettered chapters be separate from numbered chapters

--- tracker/main.cpp
+++ tracker/main.cpp
@@ -2,16 +2,19 @@
 
 void getChapter();
 void getLesson();
+void getO();
+void getF();
 int getTotal();
 int getCompleted();
 int getLeft();
 double getPercentage();
 
-const int numOfChap{34};
-const int chap[numOfChap]{13, 12, 14, 11, 13, 10, 9, 4, 15, 16, 7, 10, 11, 5, 16, 17, 18, 11, 13, 14, 4, 5, 8, 16, 8, 8, 10, 12, 7, 11, 7, 4, 5, 1};
+const int numOfChap{32};
+const int chap[numOfChap]{13, 12, 14, 11, 13, 10, 9, 15, 16, 7, 10, 11, 16, 17, 18, 11, 13, 14, 4, 5, 8, 16, 8, 8, 10, 12, 7, 11, 7, 4, 5, 1};
 
 int chapter{};
 int lesson{};
+int letteredChaps{9};
 int total{};
 int completed{};
 int left{};
@@ -21,6 +24,8 @@ int main()
 {
     getChapter();
     getLesson();
+    getO();
+    getF();
 
     std::cout << "\nTotal lessons: " << getTotal() << '\n';
 
@@ -58,6 +63,30 @@ void getLesson()
     std::cin >> lesson;
 }
 
+void getO()
+{
+    std::cout << "\nHow much of O(0-4): ";
+    int num{};
+    std::cin >> num;
+
+    if (num > 0)
+    {
+        completed += num;
+    }
+}
+
+void getF()
+{
+    std::cout << "How much of F(0-5): ";
+    int num{};
+    std::cin >> num;
+
+    if (num > 0)
+    {
+        completed += num;
+    }
+}
+
 int getTotal()
 {
     for (int i = 0; i < numOfChap; ++i)
@@ -65,7 +94,7 @@ int getTotal()
         total += chap[i];
     }
 
-    return total;
+    return total + letteredChaps;
 }
 
 int getCompleted()
