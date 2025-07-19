5412ebc52afde03dff730fd57f2d1aa887924a4c
hardeek-sharma <hardeeksa@gmail.com>
2025-07-19 00:16:31 -0400

    answered question 7.11

---
+++ quizAnswers/chapter7/lesson11/main.cpp
@@ -0,0 +1,9 @@
+/*
+Question #1
+    What effect does using keyword static have on a global variable? 
+    What effect does it have on a local variable?
+Answer:
+    Using static on a global variable makes the variable only usable in the file it was defined also known as having internal linkage.
+    Using static on a local variable makes it a type of global variable that dies at the end of 
+    a file but is out of scope when the block it was defined in ends.
+*/
