7b0a866fabda9bd29c96381bf1ef89fc28affab2
hardeek-sharma <hardeeksa@gmail.com>
2025-07-24 22:39:38 -0400

    made q2 easier to read

--- quizAnswers/chapter8/lessonX/q3/main.cpp
+++ quizAnswers/chapter8/lessonX/q3/main.cpp
@@ -3,7 +3,9 @@ Question #3
 
 Implement a game of Hi-Lo. First, your program should pick a random integer between 1 and 100. The user is given 7 tries to guess the number.
 
-If the user does not guess the correct number, the program should tell them whether they guessed too high or too low. If the user guesses the right number, the program should tell them they won. If they run out of guesses, the program should tell them they lost, and what the correct number is. At the end of the game, the user should be asked if they want to play again. If the user doesn’t enter ‘y’ or ‘n’, ask them again.
+If the user does not guess the correct number, the program should tell them whether they guessed too high or too low. If the user guesses the right number, the program 
+should tell them they won. If they run out of guesses, the program should tell them they lost, and what the correct number is. At the end of the game, the user should be 
+asked if they want to play again. If the user doesn’t enter ‘y’ or ‘n’, ask them again.
 
 For this quiz, assume the user enters a valid number.
 
