9395f144219ec9f635d829bdc013b9531cff63dd
hardeek-sharma <hardeeksa@gmail.com>
2025-07-28 17:38:32 -0400

    answered question 10.7

---
+++ quizAnswers/chapter10/lesson7/main.cpp
@@ -0,0 +1,12 @@
+/*
+Question #1
+
+Given the following function prototype:
+        int printData();
+
+Convert the int return value to a type alias named PrintError. Include both the type alias statement and the updated function prototype.
+*/
+
+using PrintError = int;
+
+PrintError printData();
