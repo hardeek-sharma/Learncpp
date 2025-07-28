681178c468cee03a59fa64359626bdb5836e7008
hardeek-sharma <hardeeksa@gmail.com>
2025-07-28 17:10:59 -0400

    answered question 10.6

---
+++ quizAnswers/chapter10/lesson6/main.cpp
@@ -0,0 +1,8 @@
+/*
+Question #1
+
+What’s the difference between implicit and explicit type conversion?
+*/
+
+// Implicit happens automatically when a different data type than expected it used.
+// Explicit happens manually with casts when we want to change a data type for something.
