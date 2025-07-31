8eb4f2fdc5c50afcb621d3534fa3d413e56fd903
hardeek-sharma <hardeeksa@gmail.com>
2025-07-31 01:17:48 -0400

    answered question 11.6

---
+++ quizAnswers/chapter11/lesson6/main.cpp
@@ -0,0 +1,7 @@
+/*
+Question #1
+
+Describe why construction blueprints are a type of template.
+*/
+
+// Because they give you a structure whose appearance you can change whenever you use the blueprints differently.
