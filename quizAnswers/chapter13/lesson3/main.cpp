73834055a2c8627128ce79d25118ee14e9ad461f
hardeek-sharma <hardeeksa@gmail.com>
2025-08-11 12:32:33 -0400

    answered question 13.3

---
+++ quizAnswers/chapter13/lesson3/main.cpp
@@ -0,0 +1,26 @@
+/*
+Question #1
+
+True or false. Enumerators can be:
+
+Given an integer value
+// true
+
+Given no explicit value
+// true
+
+Given a floating point value
+// false
+
+Given a negative value
+// true
+
+Given a non-unique value
+// true
+
+Given the value of a prior enumerator (e.g. magenta = red)
+// true
+
+Given a non-constexpr value
+// false
+*/
